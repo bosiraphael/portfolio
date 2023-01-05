c45374193b4c8719a042f0f090e939ea242f5b8f
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-01-05 12:19:07 +0100

    created hills

--- components/LaplandScene.tsx
+++ components/LaplandScene.tsx
@@ -1,12 +1,58 @@
 import { Canvas, useThree } from "@react-three/fiber";
 import styles from "../styles/Home.module.css";
-import React from "react";
-import { Color } from "three";
+import React, { createRef, useEffect } from "react";
+import {
+  BufferAttribute,
+  BufferGeometry,
+  Color,
+  Mesh,
+  PlaneGeometry,
+} from "three";
+import { OrbitControls } from "@react-three/drei";
+import { createNoise2D } from "simplex-noise";
+import alea from "alea";
 
 const Scene = () => {
   const { scene } = useThree();
   scene.background = new Color("#c9f5ff");
   return <></>;
+};
+
+const hillsRef = createRef<Mesh>();
+
+const Hills = () => {
+  useEffect(() => {
+    const simplex = createNoise2D(alea("hello"));
+
+    const hillsGeometry = hillsRef.current?.geometry;
+
+    const position = hillsGeometry?.attributes?.position;
+    if (!position) return;
+
+    for (let i = 0; i < position.count; i++) {
+      const x = position.getX(i);
+      const y = position.getY(i);
+      const z = position.getZ(i);
+      const noise = simplex(x, y);
+      position.setZ(i, z + noise);
+    }
+
+    position.needsUpdate = true;
+    hillsGeometry.computeVertexNormals();
+  }, []);
+
+  return (
+    <mesh
+      ref={hillsRef}
+      receiveShadow
+      castShadow
+      rotation={[-Math.PI / 2, 0, 0]}
+      position={[0, -1, 0]}
+    >
+      <planeGeometry args={[10, 10, 100, 100]} />
+      <meshStandardMaterial color="white" />
+    </mesh>
+  );
 };
 
 type Props = {};
@@ -21,6 +67,7 @@ export default function LaplandScene({}: Props) {
       }}
     >
       <Scene />
+      <OrbitControls />
       <ambientLight intensity={2} />
       <directionalLight
         position={[0, 1, 1]}
@@ -34,10 +81,7 @@ export default function LaplandScene({}: Props) {
         shadow-camera-top={10}
         shadow-camera-bottom={-10}
       />
-      <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, 0]}>
-        <planeGeometry args={[100, 100, 100, 100]} />
-        <meshStandardMaterial color="white" />
-      </mesh>
+      <Hills />
     </Canvas>
   );
 }
