c50a888b5e770e16f1fb17851f5fb642fa557239
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-01-06 13:57:01 +0100

    creating forest

--- components/LaplandScene/Tree.tsx
+++ components/LaplandScene/Tree.tsx
@@ -1,14 +1,15 @@
 import { useGLTF } from "@react-three/drei";
 import { MeshStandardMaterial } from "three";
-const Tree = () => {
+const Trees = () => {
   const gltf = useGLTF("/models/spruce.glb");
 
   gltf.scene.traverse((child) => {
+    // @ts-ignore
     if (child.isMesh) {
       child.castShadow = true;
       child.receiveShadow = true;
-
       if (child.name === "leaves002") {
+        // @ts-ignore
         child.material = new MeshStandardMaterial({
           color: "white",
           roughness: 0.5,
@@ -18,9 +19,27 @@ const Tree = () => {
     }
   });
 
-  gltf.scene.position.set(0, 0, 0);
+  // Create multiple trees
+  const trees = [];
+  for (let i = 0; i < 20; i++) {
+    const tree = gltf.scene.clone();
+    tree.position.x = (Math.random() - 0.5) * 10;
+    tree.position.z = -Math.random() * 20 + 7;
+    tree.position.y = 0;
 
-  return <primitive object={gltf.scene} />;
+    const randomScale = 0.5 + Math.random() * 0.5;
+    tree.scale.set(randomScale, randomScale, randomScale);
+    trees.push(tree);
+  }
+  console.log(trees);
+
+  return (
+    <>
+      {trees.map((tree, index) => (
+        <primitive key={index} object={tree} />
+      ))}
+    </>
+  );
 };
 
-export default Tree;
+export default Trees;
