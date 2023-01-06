e47b7b208a2c03b42492229bd715de262268ee58
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-01-06 12:20:02 +0100

    making the snow look more realistic

--- components/LaplandScene/Snow.tsx
+++ components/LaplandScene/Snow.tsx
@@ -1,14 +1,21 @@
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useLoader } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
-import { BufferAttribute, BufferGeometry } from "three";
+import { BufferAttribute, BufferGeometry, TextureLoader } from "three";
 
 export default function SnowParticles() {
   const points = useRef() as any;
+  let lastElapsedTime = 0;
+  let delta = 0;
+
+  const alphaMap = useLoader(
+    TextureLoader,
+    "/textures/snowflake-alpha.png"
+  ) as any;
 
   useEffect(() => {
     if (!points.current) return;
 
-    const count = 50000;
+    const count = 10000;
     const positions = new Float32Array(count * 3);
 
     for (let i = 0; i < count * 3; i++) {
@@ -21,11 +28,15 @@ export default function SnowParticles() {
     );
   }, []);
 
-  useFrame(() => {
+  useFrame(({ clock }) => {
     const positions = points.current?.geometry.attributes.position?.array;
     if (!positions) return;
+    const elapsedTime = clock.getElapsedTime();
+    delta = elapsedTime - lastElapsedTime;
+    lastElapsedTime = elapsedTime;
+
     for (let i = 0; i < positions.length; i += 3) {
-      positions[i + 1] -= 0.02;
+      positions[i + 1] -= delta * 1.2;
       if (positions[i + 1] < 0) {
         positions[i + 1] = 10 + Math.random() * 10;
       }
@@ -36,7 +47,14 @@ export default function SnowParticles() {
   return (
     <points ref={points}>
       <bufferGeometry></bufferGeometry>
-      <pointsMaterial attach="material" color="white" size={0.05} />
+      <pointsMaterial
+        attach="material"
+        color="white"
+        size={0.1}
+        sizeAttenuation
+        alphaMap={alphaMap}
+        transparent
+      />
     </points>
   );
 }
