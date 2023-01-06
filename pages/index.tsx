b1444c921ea4b9cff790266797c798da41b15c24
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-01-06 22:38:48 +0100

    fixed warnings

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
     <>
       <Head>
         <title>Raphaël Bosi's Portfolio</title>
-        <meta name="description" content="Raphaël Bosi's Portfolio" />
+        <meta name="description" content="Raphaël Bosi&aposs Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
