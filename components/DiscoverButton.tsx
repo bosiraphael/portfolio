92c0c9bccbaaf9f58ffafde9792723d51909a412
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-01-13 13:59:28 +0100

    working on discover button

--- components/DiscoverButton.tsx
+++ components/DiscoverButton.tsx
@@ -1,35 +1,13 @@
-import {
-  Link,
-  Button,
-  Element,
-  Events,
-  animateScroll as scroll,
-  scrollSpy,
-  scroller,
-} from "react-scroll";
 const DiscoverButton = () => {
   return (
     <button
-      onClick={() => {
-        window.scrollTo({
-          top: window.innerHeight,
-          behavior: "smooth",
-        });
-      }}
-      style={{
-        position: "absolute",
-        top: "80vh",
-        left: "50%",
-        width: "15rem",
-        height: "15rem",
-        transform: "translate(-50%, -50%)",
-        cursor: "pointer",
-        fontSize: "2rem",
-        color: "black",
-        backgroundColor: "white",
-        border: "none",
-        borderRadius: "50%",
-      }}
+      // onClick={() => {
+      //   window.scrollTo({
+      //     top: window.innerHeight,
+      //     behavior: "smooth",
+      //   });
+      // }}
+      className="discover-button"
     >
       Discover
     </button>
