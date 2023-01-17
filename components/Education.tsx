cdbfbdd611edeb925cc93cfa57bfbed8b320b4b1
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-01-17 08:32:42 +0100

    quick modifications

--- components/Education.tsx
+++ components/Education.tsx
@@ -33,8 +33,9 @@ const Education = () => {
         <EducationScene modelPath="models/centraleSupelec.glb" />
         <Skill
           title="CentraleSupélec"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet vero repellat facere fuga sed alias doloribus vitae recusandae quas assumenda optio aspernatur obcaecati delectus ducimus nobis sapiente eligendi libero eum, explicabo dolore voluptas nulla praesentium. Sunt deleniti assumenda impedit fugiat atque eveniet aut! Aliquid necessitatibus incidunt dolore libero reiciendis aut! Maiores non rerum maxime laborum, perspiciatis iste ipsam sapiente commodi ab. Asperiores, maxime, consectetur, optio quae iste delectus fugiat beatae aspernatur veritatis est odit pariatur sequi iusto minima suscipit facere dolores libero similique maiores. Mollitia, quisquam! Fuga, voluptatibus! Reprehenderit animi natus ullam quod placeat tempore quas numquam nam temporibus excepturi!"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius consequatur doloremque fugit expedita eos numquam dolores suscipit et facilis. Saepe accusamus optio doloribus nesciunt? Quam voluptate pariatur, itaque illo exercitationem temporibus rerum accusantium praesentium sapiente nisi qui recusandae odit sed quidem deleniti dicta explicabo quisquam vitae ipsam id fugit."
         />
+
         <div className="education-date">
           <p>2020</p>
         </div>
@@ -42,7 +43,7 @@ const Education = () => {
         <EducationScene modelPath="models/chalmers.glb" />
         <Skill
           title="Chalmers University of Technology"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est asperiores ipsa, distinctio perspiciatis excepturi mollitia in neque qui sequi sit, repudiandae nostrum odit dolore vero velit iste recusandae iure. Consectetur voluptatum nihil corrupti fuga molestiae totam natus ipsum labore, quidem dignissimos alias facere nisi. Molestias tempora quam quisquam ab aperiam, expedita qui beatae in incidunt, sequi molestiae. Aut quo, neque excepturi vitae sunt illo architecto vero voluptatibus perspiciatis illum harum consectetur ex. Tempora facilis, praesentium fuga sapiente vitae beatae tenetur hic porro harum! Modi, non, fugiat aspernatur sequi animi cum id nam labore natus ipsam optio consectetur minus, corporis aliquid."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius consequatur doloremque fugit expedita eos numquam dolores suscipit et facilis. Saepe accusamus optio doloribus nesciunt? Quam voluptate pariatur, itaque illo exercitationem temporibus rerum accusantium praesentium sapiente nisi qui recusandae odit sed quidem deleniti dicta explicabo quisquam vitae ipsam id fugit."
         />
         <div className="education-date">
           <p>2016-2018</p>
@@ -50,7 +51,7 @@ const Education = () => {
         <EducationScene text="MPSI - MP*" />
         <Skill
           title="MPSI - MP*"
-          description="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est asperiores ipsa, distinctio perspiciatis excepturi mollitia in neque qui sequi sit, repudiandae nostrum odit dolore vero velit iste recusandae iure. Consectetur voluptatum nihil corrupti fuga molestiae totam natus ipsum labore, quidem dignissimos alias facere nisi. Molestias tempora quam quisquam ab aperiam, expedita qui beatae in incidunt, sequi molestiae. Aut quo, neque excepturi vitae sunt illo architecto vero voluptatibus perspiciatis illum harum consectetur ex. Tempora facilis, praesentium fuga sapiente vitae beatae tenetur hic porro harum! Modi, non, fugiat aspernatur sequi animi cum id nam labore natus ipsam optio consectetur minus, corporis aliquid."
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius consequatur doloremque fugit expedita eos numquam dolores suscipit et facilis. Saepe accusamus optio doloribus nesciunt? Quam voluptate pariatur, itaque illo exercitationem temporibus rerum accusantium praesentium sapiente nisi qui recusandae odit sed quidem deleniti dicta explicabo quisquam vitae ipsam id fugit."
         />
       </div>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ export default function Home() {
         <Canvas
           shadows
           camera={{
-            position: [0, 2, 11],
+            position: [0, 1, 11],
           }}
           style={{ width: "100%", height: "100vh" }}
         >
