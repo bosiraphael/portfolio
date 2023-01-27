491af73bebc3d76a2306200595a533500ab50ed7
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-01-27 10:49:50 +0100

    changing discover button

--- components/DiscoverButton.tsx
+++ components/DiscoverButton.tsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
+
 const DiscoverButton = () => {
+  const [hover, setHover] = useState(false);
   return (
     <button
       onClick={() => {
@@ -7,12 +10,15 @@ const DiscoverButton = () => {
           behavior: "smooth",
         });
       }}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
       className="discover-button"
     >
       <span
         className="material-symbols-outlined"
         style={{
-          fontSize: "5rem",
+          fontSize: hover ? "10rem" : "8rem",
+          transition: "all 0.2s ease-in-out",
         }}
       >
         expand_more
