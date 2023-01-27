4013d8fadc7a69bc0c2d6c9333d841f7e256a90e
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-01-27 11:41:01 +0100

    adding logos to the skills scenes

--- components/Skills/DataScienceScene.tsx
+++ components/Skills/DataScienceScene.tsx
@@ -1,7 +1,22 @@
 import CubeScene from "./CubeScene";
 
 const DataScienceScene = () => {
-  return <CubeScene textures={["logos/typescript.png"]} />;
+  return (
+    <CubeScene
+      textures={[
+        "logos/python.png",
+        "logos/R.png",
+        "logos/pytorch.png",
+        "logos/tensorflow.png",
+        "logos/keras.png",
+        "logos/sklearn.png",
+        "logos/pandas.png",
+        "logos/numpy.png",
+        "logos/matplotlib.png",
+        "logos/seaborn.png",
+      ]}
+    />
+  );
 };
 
 export default DataScienceScene;
--- components/Skills/WebDevelopmentScene.tsx
+++ components/Skills/WebDevelopmentScene.tsx
@@ -4,12 +4,16 @@ const WebDevelopmentScene = () => {
   return (
     <CubeScene
       textures={[
+        "logos/html.png",
+        "logos/css.png",
         "logos/javascript.png",
         "logos/typescript.png",
         "logos/react.png",
+        "logos/next.png",
         "logos/nodejs.png",
         "logos/sass.png",
         "logos/firebase.png",
+        "logos/threejs.png",
       ]}
     />
   );
