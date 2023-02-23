f809e77ee9669d7b5a92f396a114dc54e05fd4c0
bosiraphael <71827178+bosiraphael@users.noreply.github.com>
2023-02-23 10:58:12 +0100

    working on the computer screen

---
+++ components/Home/MacBookProPage.tsx
@@ -0,0 +1,40 @@
+const MacBookProPage = () => {
+  return (
+    <div style={{ width: "100%", height: "100%", fontSize: "4px" }}>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "10%",
+          alignItems: "center",
+          justifyContent: "space-around",
+        }}
+      >
+        <img src="rb.svg" alt="Raphaël Bosi's logo" height={6} width={6} />
+        <p>Hello</p>
+        <p>World</p>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          height: "90%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "50%",
+            height: "50%",
+            background: "red",
+          }}
+        >
+          <p>Content</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MacBookProPage;
